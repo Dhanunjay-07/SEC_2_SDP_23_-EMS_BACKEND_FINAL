0e8acca590e6a4bdf67a4b28a87630fd57b26514
Dhanunjay-07 <2400031518@kluniversity.in>
2026-05-02 14:40:28 +0530

    Forcefully override broken Railway DB variables with native MySQL variables via System properties

--- EVMBackend/src/main/java/com/election/evm/EvmBackendApplication.java
+++ EVMBackend/src/main/java/com/election/evm/EvmBackendApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EvmBackendApplication {
 
 	public static void main(String[] args) {
+		// Forcefully override any broken SPRING_DATASOURCE_* environment variables in Railway
+		// and map them directly to Railway's auto-injected MYSQL_* variables instead.
+		System.setProperty("spring.datasource.url", "jdbc:mysql://${MYSQLHOST:localhost}:${MYSQLPORT:3306}/${MYSQLDATABASE:evm}?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
+		System.setProperty("spring.datasource.username", "${MYSQLUSER:root}");
+		System.setProperty("spring.datasource.password", "${MYSQLPASSWORD:password}");
+
 		SpringApplication.run(EvmBackendApplication.class, args);
 	}
 
